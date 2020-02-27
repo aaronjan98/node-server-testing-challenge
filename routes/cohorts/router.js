159693056583d711b4b55c0e4dc9585eebcfb90d
aaronjan <aaronjan98@gmail.com>
2020-02-27 13:56:21 -0800

    created router.spec.js for cohorts

--- routes/cohorts/router.js
+++ routes/cohorts/router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-    res.json({ cohorts: "up" });
+    res.json({ router: "cohorts" });
 });
 
 module.exports = router;
--- routes/students/router.js
+++ routes/students/router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-    res.json({ students: "up" });
+    res.json({ router: "students" });
 });
 
 module.exports = router;
